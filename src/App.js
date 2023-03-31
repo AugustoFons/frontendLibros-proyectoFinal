1d11a39660050b19f26feadc1e675b0438d5e6cf
AugustoFons <111530986+AugustoFons@users.noreply.github.com>
2023-03-30 21:20:06 -0300

    navbar material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-        <p>hi !</p>
+        <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Logo from '../images/logo2.png'
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+        width: '20ch',
+        },
+    },
+    },
+}));
+
+export default function SearchAppBar() {
+    return (
+    <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="inherit">
+            <Toolbar>
+            <Badge  
+                color="primary"
+                noWrap
+                sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
+            >
+                <img src={Logo} alt="" width={'10%'}/>
+            </Badge>
+            <Search>
+                <SearchIconWrapper>
+                <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+            <IconButton
+                size="large"
+                edge="start"
+                color="green"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            </Toolbar>
+        </AppBar>
+    </Box>
+    );
+}
